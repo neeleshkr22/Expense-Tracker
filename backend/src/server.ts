4a1c36fe3c84fecb8c8b089c0942c52bf1484ae9
Neelesh <neeleshrana22@gmail.com>
2025-07-17 23:52:40 +0530

    feat: add CORS middleware and configure JSON parsing in server

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,6 +6,19 @@ const path = require("path");
 
 const app = express();
 
-app.listen('5000',()=>{
+app.use(
+    cors({
+        origin : process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+)
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT,()=>{
     console.log("server started")
 })
